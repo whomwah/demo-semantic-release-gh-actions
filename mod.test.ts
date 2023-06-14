80a1963a8807c0372e87d4c61bf205f8e7042fc7
Fernando Ferrara <ferrarafer@gmail.com>
2023-06-14 12:23:12 -0300

    chore: Simulate fake chore change

--- mod.test.ts
+++ mod.test.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from "https://deno.land/std@0.162.0/testing/asserts.ts";
 import { sayHello } from "./mod.ts";
 
-Deno.test("Says hello world", () => {
+Deno.test("Says hello world Test", () => {
   assertEquals(sayHello(), "Hello World CHANGE 2");
 });
