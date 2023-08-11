4613cbd7f59a8423ae1b37d91bf3ea5e2cb7baad
Bernd Konnerth <bernd.konnerth@atos.net>
2023-08-11 08:50:58 +0200

    fix: test

--- mod.test.ts
+++ mod.test.ts
@@ -2,5 +2,5 @@ import { assertEquals } from "https://deno.land/std@0.162.0/testing/asserts.ts";
 import { sayHello } from "./mod.ts";
 
 Deno.test("Says hello world", () => {
-  assertEquals(sayHello(), "Hello World");
+  assertEquals(sayHello(), "Hello World 4");
 });
